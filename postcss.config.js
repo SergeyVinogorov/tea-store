75b87c008ea7880cf8416c46cdf5811ef9d53700
Sergey Vinogorov <vinogorov.s@aicrobotics.ru>
2023-08-13 12:23:40 +0300

    correct name and postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   plugins: [
-    require('postcss-import')({ path: [path.resolve(__dirname + "/")] }),
+    require('postcss-import')({ path: [path.resolve(__dirname + '/')] }),
     require('autoprefixer'),
     require('postcss-mixins'),
     require('postcss-custom-media'),
